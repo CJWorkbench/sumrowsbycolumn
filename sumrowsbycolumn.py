32a984fc0fe4952cf7522f6f8c25ed2f88a4c49b
Jonathan Stray <jonathanstray@gmail.com>
2017-11-21 19:09:28 -0800

    Update to new API

--- sumrowsbycolumn.py
+++ sumrowsbycolumn.py
@@ -1,37 +1,19 @@
-import numpy as np
+def render(table, params):
+    groupby = params['groupby']
+    sumcolumn = params['sumcolumn']
 
-class Importable:
-    @staticmethod
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def event():
-        pass
-
-    @staticmethod
-    def render(wf_module, table):
-        groupby = wf_module.get_param_column('groupby')
-        sumcolumn = wf_module.get_param_column('sumcolumn')
-
-        if sumcolumn == '':
-            wf_module.set_ready(notify=False)
-            return table
-        elif groupby not in table.columns and groupby != '':
-            wf_module.set_error('Invalid column to group row.')
-            return table
-        elif sumcolumn not in table.columns:
-            wf_module.set_error('Invalid column for sum.')
-            return table
+    if sumcolumn == '':
+        return table
+    elif groupby != '':
+        return table
+    else:
+        if table[sumcolumn].dtype != np.float64 and table[sumcolumn].dtype != np.int64:
+            table[sumcolumn] = table[sumcolumn].str.replace(',', '')
+            table[sumcolumn] = table[sumcolumn].astype(float)
+        if groupby == '':
+            newtab = table[[sumcolumn]].sum().to_frame()
+            newtab.columns = [sumcolumn + '_sum']
         else:
-            if table[sumcolumn].dtype != np.float64 and table[sumcolumn].dtype != np.int64:
-                table[sumcolumn] = table[sumcolumn].str.replace(',', '')
-                table[sumcolumn] = table[sumcolumn].astype(float)
-            if groupby == '':
-                newtab = table[[sumcolumn]].sum().to_frame()
-                newtab.columns = [sumcolumn + '_sum']
-            else:
-                newtab = table.groupby([groupby])[[sumcolumn]].sum()
-                newtab = newtab.add_suffix('_sum').reset_index()
-            wf_module.set_ready(notify=False)
-            return newtab
+            newtab = table.groupby([groupby])[[sumcolumn]].sum()
+            newtab = newtab.add_suffix('_sum').reset_index()
+        return newtab
